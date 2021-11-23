48f7804991a528ed7b67530ce19963401286580b
monicatevy <monicatevy.sen@pm.me>
2021-11-24 00:31:52 +0700

    Phase 14 : ORM (ajout d'un enregistrement)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -40,8 +40,31 @@ class AdminVoyagesController extends AbstractController{
     }
     
     /**
+     * @Route("/admin/ajout/", name="admin.voyage.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response {
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->om->persist($visite);
+            $this->om->flush();
+            return $this->redirectToRoute('admin.voyages');
+        }
+        
+        return $this->render("admin/admin.voyage.ajout.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+        ]);
+    }
+    
+    /**
      * @Route("/admin/edit/{id}", name="admin.voyage.edit")
      * @param Visite $visite
+     * @param Request $request
      * @return Response
      */
     public function edit(Visite $visite, Request $request): Response {
